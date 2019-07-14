09c13b8fbe11fb1b21dc61b8062bd41fddf12ac9
euclidmin <euclidmin@gmail.com>
2019-07-14 10:45:15 +0900

    네이버 로그인 Test

--- webdriver.py
+++ webdriver.py
@@ -3,4 +3,9 @@ from selenium import webdriver
 driver = webdriver.Chrome('chromedriver')
 driver.implicitly_wait(1)
 
-driver.get('https://www.google.com')
+driver.get('https://nid.naver.com/nidlogin.login')
+
+driver.find_element_by_name('id').send_keys('euler73')
+driver.find_element_by_name('pw').send_keys('rhdydals73.')
+driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
+
